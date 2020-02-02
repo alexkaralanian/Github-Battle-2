24fb5355a23b75f768f9c8d041b1121cc13a9a1a
Alex Karalanian <alex.karalanian@gmail.com>
2020-02-02 13:13:32 -0500

    withHover HOC refactor

--- app/Components/Tooltip.js
+++ app/Components/Tooltip.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withHover from './withHover';
 
 const styles = {
   container: {
@@ -23,44 +24,23 @@ const styles = {
   }
 };
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hovering: false
-    };
-
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-  mouseOver() {
-    this.setState({
-      hovering: true
-    });
-  }
-
-  mouseOut() {
-    this.setState({
-      hovering: false
-    });
-  }
-
-  render() {
-    const { text, children } = this.props;
-    const { hovering } = this.state;
-    return (
-      <div
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={styles.container}
-      >
-        {hovering && <div style={styles.tooltip}>{text}</div>}
-        {children}
-      </div>
-    );
-  }
+function Tooltip({ text, children, hovering }) {
+  // with our HOC, we can pass hovering to any component
+  return (
+    <div style={styles.container}>
+      {hovering && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  );
 }
 
 Tooltip.propTypes = {
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  hovering: PropTypes.bool.isRequired
 };
+
+// HOC is a component
+// Takes in another component as an arg
+// Returns a new component
+// Returned component renders original component passed into it.
+export default withHover(Tooltip, 'hovering');
---
+++ app/Components/withHover.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FaDice } from 'react-icons/fa';
+
+// function that takes in a component and returns another component
+export default function withHover(Component, propName = 'hovering') {
+  // returning another component
+  return class WitHover extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        hovering: false
+      };
+
+      this.mouseOver = this.mouseOver.bind(this);
+      this.mouseOut = this.mouseOut.bind(this);
+    }
+    mouseOver() {
+      this.setState({
+        hovering: true
+      });
+    }
+
+    mouseOut() {
+      this.setState({
+        hovering: false
+      });
+    }
+
+    render() {
+      // To prevent naming collisions, we allow end-user to specify what the 'hovering'  propName will be.
+      // we then merge that into the remaining props object being passed from ToolTip down to withHover
+      const props = {
+        [propName]: this.state.hovering,
+        ...props
+      };
+
+      return (
+        // We pass entire updated props object down to withHowver
+        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+          <Component {...props} />
+        </div>
+      );
+    }
+  };
+}
