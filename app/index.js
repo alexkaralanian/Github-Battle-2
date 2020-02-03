423bab733250384265ec3d92bc59bd05fcafd2ca
Alex Karalanian <alex.karalanian@gmail.com>
2020-02-02 22:45:27 -0500

    Added Theme Context

--- app/index.js
+++ app/index.js
@@ -2,15 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Battle from './Components/Battle';
 import Popular from './Components/Popular';
+import Nav from './Components/Nav';
 import './index.css';
 
+import { ThemeProvider } from './contexts/theme';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }));
+      }
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        {/* <Popular /> */}
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            {/* <Popular /> */}
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
